2bc3a9d8e5822d070e2f332c1fc829cbccb69647
okolipraiz <okolipraiz@gmail.com>
2023-02-06 23:11:55 +0100

    file update

---
+++ day-1/datatypes.js
@@ -0,0 +1,37 @@
+const surName = "Praise";
+const isMale = true;
+let ageNumber;
+
+const test = null;
+
+console.log(typeof surName);
+console.log(typeof isMale);
+console.log(typeof ageNumber);
+console.log(typeof test);
+
+ 
+let ageMate, castName, isFemale, isBold;
+
+const IsAgeMate = true;
+const myLastName = false;
+const isSheFemale = true;
+const isHeBold = true;
+
+const firstName = "Pharoah";
+const lastName = "King";
+const maritalStatus = false;
+const country = "NG";
+const age = 26;
+
+let firstSurName = "Among",
+    lastSurName = "Gods",
+    maritalstatus = "taken",
+    myCountry = "GH",
+    ourAge = 23;
+
+
+const myAge = 12;
+const yourAge = 17;
+
+console.log(`I am ${myAge} years old`);
+console.log(`You are ${yourAge} years old`);
---
+++ day-1/day-1.js
@@ -0,0 +1,13 @@
+// Day 1: Excercise
+
+// comments can make code readable
+// welcome to 30DaysOfJavaScript
+/* comments can make code readable, easy to reuse and informative */
+
+const firstName = "Praise";
+const isMale = true;
+let age
+
+const test = null
+
+console.log(test)
